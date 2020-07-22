e9c51c44d1f429c94cb718da283a638e71254ca8
Casey Brown <caseyjamesbrown@outlook.com>
2020-07-22 11:11:41 +0100

    Small fixes
    Corrected Pollen_Plant script to properly display data and Plant_CO2 now uses factors instead of char

--- Trainingdata/R/Modern_Pollen_Plant_Script.R
+++ Trainingdata/R/Modern_Pollen_Plant_Script.R
@@ -6,6 +6,6 @@ Herb.data <- subset(Modern_Pollen_Plant_Diversity_Relationships, Modern_Pollen_P
 #does the same filtering but with the herbaceous data this time
 par(mar = c(5,5,3,3), mfrow = c(2,1))
 #sets margins and allows display of two graphs
-plot(x = Herb.data$Clonality, y = Herb.data$Seed.weight..mg., main = 'Does clonality affect seed weight', xlab = 'Clonality', ylab = 'Log Seed weight (mg)', pch = 19, frame = FALSE, col = 'red', xlim = c(0, 1), ylim = c(0, log10(1500)))
-plot(x = Woody.data$Clonality, y = Woody.data$Seed.weight..mg., main = 'Does clonality affect seed weight', xlab = 'Clonality', ylab = 'Log Seed weight (mg)', pch = 19, frame = FALSE, col = 'blue', xlim = c(0, 1), ylim = c(0, log(2700)))
+plot(x = Herb.data$Clonality, y = Herb.data$Seed.weight.mg, main = 'Does clonality affect seed weight', xlab = 'Clonality', ylab = 'Log Seed weight (mg)', pch = 19, frame = FALSE, col = 'red', xlim = c(0, 1), ylim = c(0, log10(1500)))
+plot(x = Woody.data$Clonality, y = Woody.data$Seed.weight.mg, main = 'Does clonality affect seed weight', xlab = 'Clonality', ylab = 'Log Seed weight (mg)', pch = 19, frame = FALSE, col = 'blue', xlim = c(0, 1), ylim = c(0, log(2700)))
 #plots both graphs in sequence on same plot
